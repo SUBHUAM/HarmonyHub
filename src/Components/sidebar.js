aba74b82aa79a915ac49121b7c9925a90dffa496
SUBHUAM <subhuamranger@gmail.com>
2024-01-16 01:16:03 +0530

    Feed changes

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -8,21 +8,10 @@ import apiclient from '../spotify';
 export default function Sidebar() {
 
 
-  // useEffect(()=>{
-
-  //   apiclient.get("me").then(response=>{
-      
-  //   })
-  // },[])
 
   return (
     <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col text-white shadow-lg bg-purple-900'>
-    
-    <Barbuttons title="Feed" to="/feed" icon={<MdSpaceDashboard/>}/>
     <Barbuttons title="library" to="/" icon={<IoLibrary/>}/>
-   
-    
-
     </div>
   )
 }
